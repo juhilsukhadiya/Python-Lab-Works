7d52182fc902e43cb119ac3a72d8f96f93c5dc5d
juhilsukhadiya <juhilsukhadiya.com>
2026-08-14 12:58:28 +0530

    Simple ex

---
+++ Ex1.py
@@ -0,0 +1,41 @@
+#arithmatic operator
+
+a=3
+b=4
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a**b)
+
+#assigment operator
+
+a=2
+
+a += 3
+print(a)
+
+a -= 2
+print(a)
+
+a *= 3
+print(a)
+
+a /= 3
+print(a)
+
+
+#comparision operator
+
+a=21
+b=22
+
+print(a == b)
+print(a != b)
+print(a <= b)
+print(a >= b)
+print(a < b)
+print(a > b)
+
